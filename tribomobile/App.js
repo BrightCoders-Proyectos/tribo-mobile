5560a75207bf4917d0e74d865354e944b0449cd5
Daniel Sanchez <eliasdanielsanchezlopez@gmail.com>
2020-12-02 13:45:04 -0600

    set up
    Co-Authored-By: alcamk <alvarezohc@gmail.com>

---
+++ tribomobile/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Hola Tribo App</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
