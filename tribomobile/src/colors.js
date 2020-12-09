4c72a0d3fd88c011e1eafbccc0a40dec3a0aea64
Nanci Lizbeth García Martínez <62182513+NanciGarcia@users.noreply.github.com>
2020-12-09 01:35:44 -0600

    Create colors.js
    Create colors scripts

---
+++ tribomobile/src/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  white: '#ffffff',
+  BlueLight: '#12a4ed',
+  BlueDark: '#0b0f34',
+  OrangeFonts: '#e98d1e',
+  OrangeDark: '#fe8500',
+  GreenContent: '#7ad520',
+  GrayLight: '#dfe0e5',
+  GrayDark: '#9b9eb1',
+};
+export default colors;
