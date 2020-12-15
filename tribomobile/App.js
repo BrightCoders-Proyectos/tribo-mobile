cf18b15de5e400cac4726e4b09d2ab2a3b47212e
Héber <ing.heberh@gmail.com>
2020-12-15 14:56:28 -0600

    Created Readme Screens UI

--- tribomobile/App.js
+++ tribomobile/App.js
@@ -10,17 +10,14 @@ import React from 'react';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
 import ModalInfoStore from './components/modals/ModalInfoStore';
 import LittlePin from './components/modals/LittlePinInfo';
-import titlesText from './src/titlesText';
-import contentText from './screensText/contentText';
+import RecomendationScreen from './screens/recomendationScreen';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ModalInfoStore />
-        <Text>Hola Tribo App</Text>
-        <LittlePin />
+        <RecomendationScreen />
       </SafeAreaView>
     </>
   );
