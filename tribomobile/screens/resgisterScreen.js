740404da4288b4ffa1e0b6636addf49a448f2ae6
Jordi Garcia <jordi@tokyoplay.com>
2020-12-17 17:58:48 -0600

    ading RegisterScreen screen

---
+++ tribomobile/screens/resgisterScreen.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import titlesText from '../../src/titlesText';
+
+
+function RegisterScreen(){
+    //const [modalVisible, setModalVisible] = useState(false);
+    return(
+        <>
+
+        </>
+    );
+
+}
+
+export default RegisterScreen;
