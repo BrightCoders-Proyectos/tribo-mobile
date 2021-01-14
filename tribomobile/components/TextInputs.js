cf038c1116e8cfdfbbfc3fb11f113ece85ff58e9
Héber <ing.heberh@gmail.com>
2021-01-14 10:59:45 -0600

    Create TextInputCompoent and add corresponidng color styles

---
+++ tribomobile/components/TextInputs.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Colors from '../src/Colors';
+
+const TextInputs = (props) => {
+  const {textInputType, placeholderText} = props;
+
+  const textInputStyling = (textInputType) => {
+    let styleProperties = [];
+    switch (textInputType) {
+      case 'searchBar':
+        styleProperties = [styles.container, styles.searchBar];
+        break;
+      case 'textInputNull':
+        styleProperties = [
+          styles.container,
+          styles.inputTextForm,
+          styles.textInputError,
+        ];
+        break;
+      case 'textInputDisabled':
+        styleProperties = [
+          styles.container,
+          styles.inputTextForm,
+          styles.textInputDisabled,
+        ];
+        break;
+      default:
+        styleProperties = [styles.container, styles.inputTextForm];
+        break;
+    }
+
+    return styleProperties;
+  };
+
+  return (
+    <>
+      <TextInput
+        style={textInputStyling(textInputType)}
+        placeholder={placeholderText}
+        placeholderTextColor={
+          textInputType === 'textInputNull' ? Colors.Red : Colors.GrayDark
+        }
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+    borderRadius: 50,
+    backgroundColor: Colors.BlueSky,
+    color: Colors.BlueDark,
+    margin: 10,
+    width: '90%',
+  },
+  searchBar: {
+    height: 50,
+    paddingLeft: 25,
+    paddingRight: 25,
+    width: '80%',
+  },
+  inputTextForm: {
+    justifyContent: 'center',
+    height: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  textInputError: {
+    borderWidth: 1,
+    borderColor: Colors.Red,
+  },
+  textInputDisabled: {
+    backgroundColor: Colors.SpaceGray,
+  },
+  active: {
+    borderColor: Colors.BlueDark,
+    borderWidth: 1,
+  },
+});
+
+export default TextInputs;
