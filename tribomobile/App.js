6235e7d17d1225868b4f79b6afbf0572f0d28492
oswaldotovar32 <oswaldotovar32@gmail.com>
2021-01-21 00:02:39 -0600

    remove comments

--- tribomobile/App.js
+++ tribomobile/App.js
@@ -13,15 +13,6 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-// import SideNavScreen from './screens/SideNavScreen';
-// // import titlesText from './src/TitlesText';
-// import LittlePin from './components/modals/LittlePinInfo';
-// import WelcomeScreen from './components/screens/welcome/WelcomeScreen';
-// import WelcomeGuestScreen from './components/screens/welcome/WelcomeGuestScreen';
-// import RecomendationScreen from './screens/recomendationScreen';
-// import RegistrationScreen from './screens/registrationScreen';
-// import ContentText from './screensText/ContentText';
-// import SplashScreen from './screens/SplashScreen';
 
 const App: () => React$Node = () => {
   return (
