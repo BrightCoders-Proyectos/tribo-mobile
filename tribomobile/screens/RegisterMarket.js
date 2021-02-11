f2b2d89035a098165ccdda44ee0e8c5b2dbfbaea
Jordi Garcia <jordi@tokyoplay.com>
2021-02-11 11:58:53 -0600

    borrar flex:1 arreglo muchas cosas xD

--- tribomobile/screens/RegisterMarket.js
+++ tribomobile/screens/RegisterMarket.js
@@ -6,7 +6,7 @@ import TextInputs from '../components/TextInputs';
 
 const RegisterMarKet = (props) => {
   return (
-    <View style={{flex: 1, width: '100%', height: '100%'}}>
+    <View style={{width: '100%', height: '100%'}}>
       <View style={{width: '100%', height:100, marginTop:20}}>
         <LogoTribo
           width={150}
@@ -15,15 +15,17 @@ const RegisterMarKet = (props) => {
           sideNav={false}
         />
       </View>
-      <View style={{backgroundColor:'red',flex: 1, marginTop:50, alignItems:'center',flexDirection:'column',height: 500}}>
-            <Titles titleType="screenTitle" title="Agrega tu negocio" />
-            <Titles txtAlign="center" titleType="inputTitle" title="Cuéntanos sobre tu negocio y empieza a conectar con tus clientes" />
+      <View style={{marginTop:50,padding:20}}>
+            <View style={{alignItems:'center'}}>
+                <Titles titleType="screenTitle" title="Agrega tu negocio" />
+                <Titles txtAlign="center" titleType="inputTitle" title="Cuéntanos sobre tu negocio y empieza a conectar con tus clientes" />
+            </View>
             <View style={{marginTop:30}}>
-                <Titles txtAlign="center" titleType="inputTitle" title="Como se llama tu negocio"/>
+                <Titles txtAlign="left" titleType="inputTitle" title="Como se llama tu negocio"/>
                 <TextInputs textInputType="searchBar" placeholderText="e.g Abarrotes Carranza"/>
             </View>
             <View style={{marginTop:10}}>
-                <Titles txtAlign="center" titleType="inputTitle" title="Telefono de negocio"/>
+                <Titles txtAlign="left" titleType="inputTitle" title="Telefono de negocio"/>
                 <TextInputs textInputType="searchBar" placeholderText="312 145 9032"/>
             </View>
         </View>
